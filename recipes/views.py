6fbe06b63b2d7ee668f1d4520629937d5853291e
Geovani Cordeiro <geovanicordeiro13@gmail.com>
2024-04-12 09:20:26 -0300

    implementing features

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# HTTP REQUEST
 def home(request):
-    return render(request, 'home.html')
-
-# urlpatterns = [('admin/', admin.site.urls),
-#     path('', home), #Home
-#     path('sobre/', sobre), #/sobre/
-#     path('contato/', contato) #/contato/
-# ]
+    return render(request, 'recipes/home.html')
